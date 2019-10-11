fe4f6e1022828c508d665f54fb3ac8818d1626eb
Charles Collins <coll5631@stthomas.edu>
2019-10-11 14:52:45 -0500

    Converted score card to be within a frame and removed radio buttons from non-assignable score fields

--- score_card.py
+++ score_card.py
@@ -1,42 +1,47 @@
 from tkinter import *
 from tkinter import ttk as tk
 
-score_card = Tk()
-
+yahtzee_gui = Tk()
+score_card = tk.Frame(yahtzee_gui)
+score_card.pack()
 assign_to = StringVar()
 
 
-def create_score_box(lbl):
+def create_score_box(lbl, can_assign):
+
+    if can_assign:
+        selector = tk.Radiobutton(score_card, variable=assign_to, text=lbl, width=17, value=lbl)
+    else:
+        selector = tk.Label(score_card, text=lbl, width=17)
+
     return [
-            tk.Radiobutton(score_card, variable=assign_to, text=lbl, width=14, value=lbl),
-            Entry(score_card, width=3),
-            Entry(score_card, width=3)
+            selector,
+            Entry(score_card, width=3, state='disabled')
         ]
 
 
-
 points = {
-    "UPPER SECTION": [Label(score_card, text="UPPER SECTION")],
-    'Aces': create_score_box('Aces'),
-    'Twos': create_score_box('Twos'),
-    "Threes": create_score_box('Threes'),
-    "Fours": create_score_box('Fours'),
-    "Fives": create_score_box('Fives'),
-    "Sixes": create_score_box('Sixes'),
-    "TOTAL SCORE": create_score_box('TOTAL SCORE'),
-    "BONUS": create_score_box('BONUS'),
-    "TOTAL": create_score_box('TOTAL'),
-    "LOWER SECTION": [Label(score_card, text="LOWER SECTION")],
-    "3 of a kind": create_score_box('3 of a kind'),
-    "4 of a kind": create_score_box('4 of a kind'),
-    "Full House": create_score_box('Full House'),
-    "Sm. Straight": create_score_box('Sm. Straight'),
-    "Lg. Straight": create_score_box('Lg. Straight'),
-    "YAHTZEE": create_score_box('YAHTZEE'),
-    "Chance": create_score_box('Chance'),
-    "Total (Lower)": create_score_box('Total (Lower)'),
-    "Total (Upper)": create_score_box('Total (Upper)'),
-    "GRAND TOTAL": create_score_box('GRAND TOTAL'),
+    "UPPER SECTION": [tk.Label(score_card, text="UPPER SECTION")],
+    'Aces': create_score_box('Aces', True),
+    'Twos': create_score_box('Twos', True),
+    "Threes": create_score_box('Threes', True),
+    "Fours": create_score_box('Fours', True),
+    "Fives": create_score_box('Fives', True),
+    "Sixes": create_score_box('Sixes', True),
+    "TOTAL SCORE": create_score_box('TOTAL SCORE', False),
+    "BONUS": create_score_box('BONUS', False),
+    "TOTAL": create_score_box('TOTAL', False),
+    "LOWER SECTION": [tk.Label(score_card, text="LOWER SECTION")],
+    "3 of a kind": create_score_box('3 of a kind', True),
+    "4 of a kind": create_score_box('4 of a kind', True),
+    "Full House": create_score_box('Full House', True),
+    "Sm. Straight": create_score_box('Sm. Straight', True),
+    "Lg. Straight": create_score_box('Lg. Straight', True),
+    "YAHTZEE": create_score_box('YAHTZEE', True),
+    "Chance": create_score_box('Chance', True),
+    "Total (Lower)": create_score_box('Total (Lower)', False),
+    "Total (Upper)": create_score_box('Total (Upper)', False),
+    "GRAND TOTAL": create_score_box('GRAND TOTAL', False),
 }
 
 rw = 0
@@ -47,4 +52,4 @@ for element in points:
         cl = cl+1
     rw = rw + 1
 
-score_card.mainloop()
+yahtzee_gui.mainloop()
