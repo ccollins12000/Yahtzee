3d58faa95c6069b2a93ac1e1efca1f544063f529
Charles Collins <coll5631@stthomas.edu>
2019-10-16 18:15:57 -0500

    Added adding functionality to die object

--- Die.py
+++ Die.py
@@ -1,10 +1,36 @@
 import random as r
 
 
-class die:
+class Die:
     def __init__(self):
-        self.value = 0
+        self._value = 0
+        self.roll()
 
     def roll(self):
-        self.value = r.randrange(1, 7)
+        """Rolls the die"""
+        self._value = r.randrange(1, 7)
 
+    def __str__(self):
+        return "A die with value of " + str(self.value)
+
+    @property
+    def value(self):
+        return self._value
+
+    def getvalue(self):
+        return self._value
+
+    def __add__(self, other):
+        return self.value + other
+
+    def __radd__(self, other):
+        return self.value + other
+
+    def __int__(self):
+        return self.value
+
+
+
+class Dice:
+    def __init__(self):
+        self._dice = []
