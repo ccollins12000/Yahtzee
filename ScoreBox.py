b9feabe41ae050c5375a6e1f11ccdd241120764a
Charles Collins <coll5631@stthomas.edu>
2019-10-20 15:26:45 -0500

    added score card object

--- ScoreBox.py
+++ ScoreBox.py
@@ -7,6 +7,14 @@ class ScoreBox:
     def __init__(self):
         self._points = 0
         self._assigned = False
+        self._name = ''
+
+    @property
+    def name(self):
+        return self._name
+
+    def getname(self):
+        return self._name
 
     @property
     def points(self):
@@ -28,6 +36,17 @@ class ScoreBox:
 
 
 class Aces(ScoreBox):
+    def __init__(self):
+        super().__init__()
+        self._name = 'Aces'
+
+    @property
+    def name(self):
+        return self._name
+
+    def getname(self):
+        return self._name
+
     def assign_points(self, dice):
         if not self.assigned:
             points = dice.dice_roll_count(1)
@@ -39,6 +58,17 @@ class Aces(ScoreBox):
 
 
 class Twos(ScoreBox):
+    def __init__(self):
+        super().__init__()
+        self._name = 'Twos'
+
+    @property
+    def name(self):
+        return self._name
+
+    def getname(self):
+        return self._name
+
     def assign_points(self, dice):
         if not self.assigned:
             points = dice.dice_roll_count(2)*2
@@ -50,6 +80,17 @@ class Twos(ScoreBox):
 
 
 class Threes(ScoreBox):
+    def __init__(self):
+        super().__init__()
+        self._name = 'Threes'
+
+    @property
+    def name(self):
+        return self._name
+
+    def getname(self):
+        return self._name
+
     def assign_points(self, dice):
         if not self.assigned:
             points = dice.dice_roll_count(3)*3
@@ -61,6 +102,17 @@ class Threes(ScoreBox):
 
 
 class Fours(ScoreBox):
+    def __init__(self):
+        super().__init__()
+        self._name = 'Fours'
+
+    @property
+    def name(self):
+        return self._name
+
+    def getname(self):
+        return self._name
+
     def assign_points(self, dice):
         if not self.assigned:
             points = dice.dice_roll_count(4)*4
@@ -72,6 +124,17 @@ class Fours(ScoreBox):
 
 
 class Fives(ScoreBox):
+    def __init__(self):
+        super().__init__()
+        self._name = 'Fives'
+
+    @property
+    def name(self):
+        return self._name
+
+    def getname(self):
+        return self._name
+
     def assign_points(self, dice):
         if not self.assigned:
             points = dice.dice_roll_count(5)*5
@@ -83,6 +146,17 @@ class Fives(ScoreBox):
 
 
 class Sixes(ScoreBox):
+    def __init__(self):
+        super().__init__()
+        self._name = 'Sixes'
+
+    @property
+    def name(self):
+        return self._name
+
+    def getname(self):
+        return self._name
+
     def assign_points(self, dice):
         if not self.assigned:
             points = dice.dice_roll_count(6)*6
@@ -94,6 +168,17 @@ class Sixes(ScoreBox):
 
 
 class ThreeOfAKind(ScoreBox):
+    def __init__(self):
+        super().__init__()
+        self._name = 'Three of a Kind'
+
+    @property
+    def name(self):
+        return self._name
+
+    def getname(self):
+        return self._name
+
     def assign_points(self, dice):
         if not self.assigned:
             if Die.of_a_kind_size(dice) >= 3:
@@ -106,6 +191,17 @@ class ThreeOfAKind(ScoreBox):
 
 
 class FourOfAKind(ScoreBox):
+    def __init__(self):
+        super().__init__()
+        self._name = 'Four of a Kind'
+
+    @property
+    def name(self):
+        return self._name
+
+    def getname(self):
+        return self._name
+
     def assign_points(self, dice):
         if not self.assigned:
             if Die.of_a_kind_size(dice) >= 4:
@@ -118,6 +214,17 @@ class FourOfAKind(ScoreBox):
 
 
 class FullHouse(ScoreBox):
+    def __init__(self):
+        super().__init__()
+        self._name = 'Full House'
+
+    @property
+    def name(self):
+        return self._name
+
+    def getname(self):
+        return self._name
+
     def assign_points(self, dice):
         if not self.assigned:
             if Die.check_full_house(dice):
@@ -130,6 +237,17 @@ class FullHouse(ScoreBox):
 
 
 class SmallStraight(ScoreBox):
+    def __init__(self):
+        super().__init__()
+        self._name = 'Small Straight'
+
+    @property
+    def name(self):
+        return self._name
+
+    def getname(self):
+        return self._name
+
     def assign_points(self, dice):
         if not self.assigned:
             if Die.straight_size(dice) >= 4:
@@ -142,6 +260,17 @@ class SmallStraight(ScoreBox):
 
 
 class LargeStraight(ScoreBox):
+    def __init__(self):
+        super().__init__()
+        self._name = 'Large Straight'
+
+    @property
+    def name(self):
+        return self._name
+
+    def getname(self):
+        return self._name
+
     def assign_points(self, dice):
         if not self.assigned:
             if Die.straight_size(dice) >= 5:
@@ -154,6 +283,17 @@ class LargeStraight(ScoreBox):
 
 
 class Chance(ScoreBox):
+    def __init__(self):
+        super().__init__()
+        self._name = 'Chance'
+
+    @property
+    def name(self):
+        return self._name
+
+    def getname(self):
+        return self._name
+
     def assign_points(self, dice):
         if not self.assigned:
             self._points = sum(dice)
@@ -163,6 +303,17 @@ class Chance(ScoreBox):
 
 
 class Yahtzee(ScoreBox):
+    def __init__(self):
+        super().__init__()
+        self._name = 'Yahtzee'
+
+    @property
+    def name(self):
+        return self._name
+
+    def getname(self):
+        return self._name
+
     def assign_points(self, dice):
         if not self.assigned:
             if Die.of_a_kind_size(dice) == 5:
@@ -173,3 +324,58 @@ class Yahtzee(ScoreBox):
     def __str__(self):
         return 'Yahtzee: ' + str(self._points)
 
+
+class ScoreCard:
+    def __init__(self):
+        self._aces = Aces()
+        self._twos = Twos()
+        self._threes = Threes()
+        self._fours = Fours()
+        self._fives = Fives()
+        self._sixes = Sixes()
+        self._upperSum = 0
+        self._upperTotal = 0
+        self._bonus = 0
+        self._threeOfAKind = ThreeOfAKind()
+        self._fourOfAKind = FourOfAKind()
+        self._fullHouse = FullHouse()
+        self._smallStraight = SmallStraight()
+        self._largeStraight = LargeStraight()
+        self._yahtzee = Yahtzee()
+        self._chance = Chance()
+        self._lowerTotal = 0
+        self._grandTotal = 0
+        self._upper_section = [self._aces, self._twos, self._threes, self._fours, self._fives, self._sixes]
+        self._lower_section = [self._threeOfAKind, self._fourOfAKind, self._fullHouse, self._smallStraight, self._largeStraight, self._yahtzee, self._chance]
+        self._boxes = [self._aces, self._twos, self._threes, self._fours, self._fives, self._sixes, self._threeOfAKind, self._fourOfAKind, self._fullHouse, self._smallStraight, self._largeStraight, self._yahtzee, self._chance]
+
+    def calculate_points(self):
+
+        #Calculate Upper Section
+        total = 0
+        for box in self._upper_section:
+            total += box.points
+        self._upperSum = total
+
+        if total >= 63:
+            self._bonus = 35
+            total += 35
+        self._upperTotal = total
+
+        # Calculate Lower Section
+        total = 0
+        for box in self._lower_section:
+            total += box.points
+
+        self._lowerTotal = total
+
+        #Total Points
+        self._grandTotal = self._upperTotal + self._lowerTotal
+
+
+    def assign_roll(self, box_name, dice):
+        for box in self._boxes:
+            if box.name == box_name
+                box.assign_points(dice)
+                break
+        self.calculate_points()
