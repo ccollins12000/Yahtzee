c37102088a315661e7d6b959003e48fcc5b4e0cf
Charles Collins <coll5631@stthomas.edu>
2019-11-11 19:48:15 -0600

    more code cleanup

--- YahtzeeModel.py
+++ YahtzeeModel.py
@@ -1,9 +1,24 @@
+"""Model for Yahtzee game
+
+This module contains the logic code for a yahtzee game
+
+"""
+
 from DieModel import *
 from ScoreCardModel import *
 
 
 class YahtzeeModel:
+    """All the logic code for running a yahtzee game
+
+    """
     def __init__(self):
+        """
+        The constructor for a yahtzee game
+
+        Notes:
+            After the game is constructed.  Use the add_player method to add players and the start_game method to begin the game.
+        """
         self._players = []
         self._game_started = False
         self._game_complete = False
@@ -13,16 +28,28 @@ class YahtzeeModel:
         self._dice = []
         self._winner = None
         self._assigned_roll = False
+        self._instructions = ''
         for die_index in range(5):
             # self._dice.append(Die())
             self._dice.append(Die())
 
     @property
+    def instructions(self):
+        """str: The instructions for the player on what to do next"""
+        return self._instructions
+
+    @instructions.setter
+    def instructions(self, value):
+        self._instructions = value
+
+    @property
     def game_over(self):
+        """bool: whether or not the game is over"""
         return self._game_complete
 
     @property
     def current_player(self):
+        """obj: the current player object"""
         player_index = self._turn % len(self._players)
         if len(self._players) > 0 and self._turn > 0:
             return self._players[player_index]
@@ -31,15 +58,24 @@ class YahtzeeModel:
 
     @property
     def rolls_remaining(self):
-        """Get the number of rolls remaining for the current turn"""
+        """int: Get the number of rolls remaining for the current turn"""
         return self._rolls_remaining
 
     @property
     def turn(self):
-        """Return the number of turns remaining in the game"""
+        """int: Return the number of turns remaining in the game"""
         return self._turn
 
     def start_game(self):
+        """Starts the game
+
+        Notes:
+            The must be players to start the game.  the add_players method can be used to add players
+
+        Raises:
+            Exception: If there are no players in the game yet
+
+        """
         self._game_complete = False
         if len(self._players) > 0:
             self._turn = len(self._players) * 13
@@ -49,16 +85,38 @@ class YahtzeeModel:
             raise Exception('There must be at least one player to start the game.')
 
     def add_player(self, player):
+        """Add a player to the game
+
+        Args:
+            player (obj): The player object to add to the game
+        """
         self._players.append(player)
 
     def remove_player(self, player_index):
+        """Removes the player at the given index from the game
+
+        Args:
+            player_index (int): The index of the player to remove
+
+        Returns:
+            The player object removed from the game
+        """
         return self._players.pop(player_index)
 
     def get_dice(self):
+        """Gets the current values of the dice
+
+        Returns:
+            A list containing all of the values of the dice
+        """
         return [die.value for die in self._dice]
 
     def roll_dice(self):
-        """Roll the dice that have been selected"""
+        """Rolls all the dice that have been selected
+
+        Returns:
+            A list containing all of the values of the dice
+        """
         if self._rolls_remaining > 0 and not self._assigned_roll:
             for die_index, die in enumerate(self._dice):
                 if die.selected:
@@ -67,6 +125,18 @@ class YahtzeeModel:
         return self.get_dice()
 
     def assign_roll(self, box_name):
+        """Assigns the current roll to the score box given
+
+        Note:
+            Assigning to a box that already had a roll assigned will do nothing
+
+        Args:
+            box_name (str): The name of the box to assign the roll to. Possible values are the following:
+            'Aces', 'Twos', 'Threes', 'Fours', 'Fives', 'Sixes', '3 of a Kind', '4 of a Kind', 'Full House', 'Small Straight', 'Large Straight', 'Yahtzee', 'Chance'
+
+        Returns:
+            The resulting points or if the box already is assigned the current points in the box
+        """
         score_card = self.current_player.score_card
         # only assign roll if the box doesn't already have value and the roll hasn't already been assigned this turn
         if self._assigned_roll or score_card.get_box_assigned(box_name) or not self._game_started:
@@ -77,6 +147,9 @@ class YahtzeeModel:
             return score_card.get_box_points(box_name)
 
     def setup_turn(self):
+        """A helper function for setting up a turn
+
+        """
         # Setup and roll dice
         self._rolls_remaining = 3
         self._assigned_roll = False
@@ -85,7 +158,9 @@ class YahtzeeModel:
         self.roll_dice()
 
     def next_turn(self):
-        """Proceed to next turn"""
+        """Moves the game to the next turn.  If it is the last turn, ends the game.
+
+        """
         if self._game_started and not self._game_complete and self._assigned_roll == True:
             self._turn = self._turn - 1
             if self._turn > 0:
@@ -94,7 +169,7 @@ class YahtzeeModel:
                 self.end_game()
 
     def end_game(self):
-        """End the game and calculate winner"""
+        """Ends the game and calculates winner"""
         self._game_started = False
         self._game_complete = True
 
