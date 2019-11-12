b4f857cff5d31ae60c783243de89aeade87e2bba
Charles Collins <coll5631@stthomas.edu>
2019-11-12 17:35:33 -0600

    Documented die view

--- DieView.py
+++ DieView.py
@@ -1,3 +1,7 @@
+"""This is a module contains all the code for displaying a die object
+
+"""
+
 from tkinter import *
 from tkinter import ttk as tk
 
@@ -6,19 +10,35 @@ class DieView:
     This is a class to display a single die that can be selected
 
     Attributes:
-        selected (bool): Whether or not the die is selected
-        last_roll (int): The number diplayed on the die
-
+        view (obj:): The Tkinter label object containing the die image
     """
     def __init__(self, master, initial_roll=6):
+        """
+        The constructor for a die object
+
+        Args:
+            master (obj): The Tkinter master object the die will be displayed in
+            initial_roll (int:) The initial display for the die object.  Default of 6
+
+        Raises:
+            Exception: If the value is not between 1 and 6, an error is raised.  This isn't some fancy D&D game, it's just a boring old Yahtzee game!
+        """
         self._last_roll = initial_roll
         self._image = PhotoImage(file="Die" + str(self._last_roll) + "_selected.png")
         self._selected = IntVar(value=1)
         self.view = tk.Label(master, image=self._image)
-        #self.view = tk.Checkbutton(master, image=self._image, variable=self._selected)
         self.view.bind("<Button-1>", self.toggle_selected)
 
     def set_image(self, value, selected):
+        """Helper function for changing the die roll value to display
+
+        Note:
+            Use toggle selected and last roll to update the die.  This is just a helper function for within the class
+
+        Args:
+            value (int): The value to display on the die
+            selected (bool): Whether or not the die shows as selected
+        """
         if selected == 1:
             self._image = PhotoImage(file="Die" + str(value) + "_selected.png")
 
@@ -27,6 +47,14 @@ class DieView:
         self.view.configure(image=self._image)
 
     def toggle_selected(self, event):
+        """Helper function for changing whether the die shows as selected or not
+
+        Note:
+            Don't utilize this function.  It is what is bound to the image clicked event
+
+        Args:
+            event (obj): A Tkinter event triggering unselecting or selecting the die.
+        """
         if self._selected.get() == 1:
             self._selected.set(0)
         else:
@@ -36,7 +64,7 @@ class DieView:
 
     @property
     def selected(self):
-        """Get or set whether the die is selected. Toggles the checkbox control"""
+        """bool: Get or set whether the die is selected."""
         return self._selected.get()
 
     @selected.setter
@@ -46,7 +74,7 @@ class DieView:
 
     @property
     def last_roll(self):
-        """Get or set the number displayed on the die"""
+        """int: Get or set the number displayed on the die"""
         return self._last_roll
 
     @last_roll.setter
